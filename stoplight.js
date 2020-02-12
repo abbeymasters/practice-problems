2530040aa6996d8aa7a1301ea2f138cde6e110a0
Abbey Masters <abbeymasters@Abbeys-MacBook-Pro.local>
2020-02-11 19:55:50 -0800

    stoplight

---
+++ stoplight.js
@@ -0,0 +1,28 @@
+const goButton = document.querySelector('button');
+const greenLight = document.querySelector('.one');
+const yellowLight = document.querySelector('.two');
+const redLight = document.querySelector('.three');
+
+const lightArray = [greenLight, yellowLight, redLight];
+const color = ['green', 'yellow', 'red'];
+let index = 1;
+
+goButton.addEventListener('click', () => {
+  index++;
+  if(index === 0) {
+    lightArray[index].classList.add('green');
+    lightArray[1].classList.remove('yellow');
+    lightArray[2].classList.remove('red');
+  }
+  if(index === 1) {
+    lightArray[index].classList.add('yellow');
+    lightArray[0].classList.remove('green');
+    lightArray[2].classList.remove('red');
+  }
+  if(index === 2) {
+    lightArray[index].classList.add('red');
+    lightArray[0].classList.remove('green');
+    lightArray[1].classList.remove('yellow');
+    index = -1;
+  }
+});
