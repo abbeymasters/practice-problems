6c7479bddf7f1192cbcb3ae355b688c49a234ed6
Abbey Masters <abbeymasters@Abbeys-MacBook-Pro.local>
2020-04-03 12:00:48 -0700

    working on regex

--- reverse-string/reverse-string.js
+++ reverse-string/reverse-string.js
@@ -1,3 +1,12 @@
-export default function reverseString(str) {
-  
+function reverseString(str) {
+  const splitWords = str.toString().split(' ');
+  let newArray = [];
+  for(let i = splitWords.length - 1; i >=0; i--) {
+    newArray.push(splitWords[i]);
+  }
+  let regex = /[,{2}]/g;
+  console.log(newArray.join().replace(regex, ' '));
 }
+
+
+module.exports = reverseString;
