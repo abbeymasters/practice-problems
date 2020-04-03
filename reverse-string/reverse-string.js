642a992de35513e358b95367b4d59d02c69a654a
Abbey Masters <abbeymasters@Abbeys-MacBook-Pro.local>
2020-04-03 13:40:43 -0700

    finished code challenge

--- reverse-string/reverse-string.js
+++ reverse-string/reverse-string.js
@@ -4,8 +4,7 @@ function reverseString(str) {
   for(let i = splitWords.length - 1; i >=0; i--) {
     newArray.push(splitWords[i]);
   }
-  let regex = /[,{2}]/g;
-  console.log(newArray.join().replace(regex, ' '));
+  return newArray.join().replace(/,,/g, ' ').replace(/,/g, '');
 }
 
 
