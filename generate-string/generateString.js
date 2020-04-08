8dd1085cd8c1180f5c86b3396b5e7306332d1efd
Abbey Masters <abbeymasters@Abbeys-MacBook-Pro.local>
2020-04-08 09:46:39 -0700

    adding new problem

---
+++ generate-string/generateString.js
@@ -0,0 +1,10 @@
+
+const generateTheString = function(n) {
+  // check if n % 2 === 0 and if so subtract one
+  // loop through and add characters to equal n - 1
+  // add one extra letter
+
+  
+};
+
+export default generateTheString;
